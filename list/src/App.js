895108db8ea3234d6122de6c34b6d4c5258f95f0
Jorel I. Agustin <106132941+AyJayyyyy@users.noreply.github.com>
2024-02-10 13:32:10 +0800

    final commit

--- list/src/App.js
+++ list/src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import EmployeeForm from './Components/EmployeeForm';
+
+function EmployeeList(props) {
+  // Render the employee list
+  return (
+    <div className="employee-list">
+      <ul>
+        {props.employees.map((employee) => (
+          <li key={employee.EmployeeId}>
+            {/* Create a link to the employee detail page */}
+            <Link to={`/employees/${employee.EmployeeId}`}>
+              {employee.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function LocalStoragePreview() {
+  const [storedData, setStoredData] = useState([]);
+
+  useEffect(() => {
+    // Retrieve data from local storage when component mounts
+    const data = JSON.parse(localStorage.getItem('employees'));
+    if (data) {
+      setStoredData(data);
+    }
+  }, []); // Add empty dependency array to run once on mount only
+
+  return (
+    <div>
+      <h1>Employee List</h1>
+
+      <ul>
+        {storedData.map((employee, index) => (
+          <li key={index}>
+            Name: {employee.name}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
 
 function App() {
+  const [employees, setEmployees] = useState([]);
+
+  // Function to add a new employee to the state variable employees
+  const addEmployee = (employee) => {
+    // Update state
+    setEmployees(prevEmployees => [...prevEmployees, employee]);
+    
+    // Save data to local storage after adding employee
+    localStorage.setItem('employees', JSON.stringify([...employees, employee]));
+  };
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <EmployeeForm addEmployee={addEmployee} />
+      <EmployeeList employees={employees} />
+      <LocalStoragePreview />
     </div>
   );
 }
